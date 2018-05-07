506d70d6cf05a3cd8ec9093d9434079430c99add
studentuser <uceskai@ucl.ac.uk>
2018-05-07 18:53:20 -0400

    changed variable names in GET

--- appActivity.js
+++ appActivity.js
@@ -6,10 +6,14 @@ function replaceGraphs() {
 			document.getElementByld("graphdiv").innerHTML="<img src=' images/ucl.png'>"
 }
 
+// define global variable for user location
+var userLoc
+
 // track user location. Code sourced from: https://stackoverflow.com/questions/10563789/how-to-locate-user-with-leaflet-locate
 function trackLocation(){
 mymap.locate({setView: true, watch: true}) /* This will return map so you can do chaining */
         .on('locationfound', function(e){
+			var userLoc = L.marker([e.latitude, e.longitude])
             var marker = L.marker([e.latitude, e.longitude]).bindPopup('Your are here').openPopup();
             var circle = L.circle([e.latitude, e.longitude], e.accuracy/2, {
                 weight: 1,
@@ -24,4 +28,6 @@ mymap.locate({setView: true, watch: true}) /* This will return map so you can do
             console.log(e);
             alert("Location access denied.");
         });
+
 };
+
