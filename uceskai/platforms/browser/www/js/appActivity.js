3f2365168ad83c57175b60c6ed7b39f9684bddc0
studentuser <uceskai@ucl.ac.uk>
2018-05-09 08:41:25 -0400

    added in-code documentation and a separate user guide

--- uceskai/platforms/browser/www/js/appActivity.js
+++ uceskai/platforms/browser/www/js/appActivity.js
@@ -2,13 +2,7 @@ function menuClicked() {
 			alert("You clicked on the menu");
 }
 
-function replaceGraphs() {
-			document.getElementByld("graphdiv").innerHTML="<img src=' images/ucl.png'>"
-}
-
-
-
-// track user location. Code sourced from: https://stackoverflow.com/questions/10563789/how-to-locate-user-with-leaflet-locate
+// track user location. Code sourced from: https://stackoverflow.com/questions/10563789/how-to-locate-user-with-leaflet-locate, accessed 7th May 2018
 function trackLocation(){
 mymap.locate({setView: true, watch: true}) /* This will return map so you can do chaining */
         .on('locationfound', function(e){
--- uceskai/www/js/uploadData.js
+++ uceskai/www/js/uploadData.js
@@ -1,12 +1,14 @@
+// function to start data Upload of question and answer selection
 function startDataUpload() {
+	// implement alert to confirm upload
 	alert ("start data upload");
-
+	// assign data to variables
 	var question = "'Head Question'";
 	var correct = document.getElementById("correct").value;
 	var answer1 = document.getElementById("answer1").value;
 	var answer2 = document.getElementById("answer2").value;
 	var answer3 = document.getElementById("answer3").value;
-	
+	// create post string
 	var postString = "&question= Whose head in main cloisters?"+question + "&correct="+correct +"&answer1="+answer1 +"&answer2="+answer2 +"&answer3="+answer3;
 	//get answer (radio buttons)
 	if (document.getElementById("answer1").checked) {
@@ -21,22 +23,25 @@ function startDataUpload() {
 	if (document.getElementById("answer4").checked) {
 		postString=postString+"&answer=Joe Tribbiani";
 	}	
+	// call function to process data
 	processData(postString);
 	
 	alert (postString);
 }
 
 var client;
+// function to process data
 function processData(postString) {
 	client = new XMLHttpRequest();
+	// call function /submitQuestion
 	client.open('POST','http://developer.cege.ucl.ac.uk:30299/submitQuestion',true);
 	client.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	client.onreadystatechange = dataUploaded;
 	client.send(postString);
 }
-// create the code to wait for the response from the data server, and process the response once it is received
+// wait for the response from data server, process the response once it is received
 function dataUploaded() {
-	// this function listens out for the server to say that the data is ready - i.e. has state 4
+	// listen out for the server to say that the data is ready - i.e. has state 4
 	if (client.readyState == 4) {
 	// change the DIV to show the response
 		document.getElementById("dataUploadResult").innerHTML = client.responseText;
